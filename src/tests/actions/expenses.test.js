e1b9971e5928267caff6ce8d534e33843f21420b
Nabu <alakkat.n@gmail.com>
2020-10-29 15:13:50 -0400

    connected editExpense to firebase

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -7,7 +7,8 @@ import {
     removeExpense,
     setExpenses,
     startSetExpenses,
-    startRemoveExpense
+    startRemoveExpense,
+    startEditExpense
 } from '../../actions/expenses'
 import expenses from '../fixtures/expenses'
 import database from '../../firebase/firebase'
@@ -55,6 +56,26 @@ test('should set up edit expense action object', () => {
         updates:{
             note:'new note value'
         } 
+    })
+})
+
+test('should edit expenses from firebase', (done) => {
+    const store = createMockStore({})
+    const id = expenses[1].id
+    const updates = {
+        description: 'Updated Description'
+    }
+    store.dispatch(startEditExpense(id, updates)).then(() => {
+        const actions = store.getActions()
+        expect(actions[0]).toEqual({
+            type: 'EDIT_EXPENSE',
+            id,
+            updates
+        })
+        return database.ref(`expenses/${id}`).once('value')
+    }).then((snapshot)=>{
+        expect(snapshot.val().description).toBe(updates.description)
+        done()
     })
 })
 
